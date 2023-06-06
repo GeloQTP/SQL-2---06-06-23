45439262917d7f791616cd1c6ca0094650f696e2
Ernest Del Rosario <geloadrian24@gmail.com>
2023-06-06 18:42:34 +0800

    Add project files.

---
+++ SQL(2)/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQL_2_;
+
+namespace DBSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new EnrollmentDBEntities2())
+            {
+
+                var query = from b in db.Students
+                            orderby b.Firstname
+                            select b;
+
+                Console.WriteLine("All All student in the database:");
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Firstname + " " + item.Lastname);
+                }
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+
+
+
+                string ConnectionString = "Integrated Security=SSPI;" +
+         "Initial Catalog=EnrollmentDB;" +
+         "Data Source = DESKTOP-EPRCMKJ\\SQLEXPRESS";
+
+
+
+                SqlConnection conn2 = new SqlConnection(ConnectionString);
+                conn2.Open();
+                using (SqlCommand cmd2 = new SqlCommand("insertNewStudent", conn2))
+                {
+                    cmd2.CommandType = CommandType.StoredProcedure;
+
+
+
+                    cmd2.Parameters.Add("@Firstname", SqlDbType.VarChar).Value = "Jan Mr TRC!";
+                    cmd2.Parameters.Add("@Lastname", SqlDbType.VarChar).Value = "Camacho";
+
+                    cmd2.ExecuteNonQuery();
+                }
+
+            }
+        }
+    }
+}
